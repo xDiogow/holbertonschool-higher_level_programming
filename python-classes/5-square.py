a04e95f4efb2a6f68a2c337146fc071cc48ea076
Diogo <diogoanonimos@gmail.com>
2025-03-17 15:31:07 +0100

    Added 5-square.py

---
+++ python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Module for class Square
+"""
+
+
+class Square:
+    """Class Square"""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def my_print(self):
+        if self.size <= 0:
+            return
+
+        for i in range(0, self.size):
+            print('#' * self.size)
+
+    def area(self):
+        return self.__size * self.__size
