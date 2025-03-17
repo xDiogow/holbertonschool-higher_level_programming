b78cca3597cb1435280670d8a5f2359bdbec02e4
Diogo <diogoanonimos@gmail.com>
2025-03-17 11:27:03 +0100

    5-print_comb2.py

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    print('{0:02}'.format(i), end='')
     if i != 99:
-        print(', ', end='')
-print()
+        print('{0:02}, '.format(i), end='')
+    else:
+        print('{0:02}\n'.format(i))
