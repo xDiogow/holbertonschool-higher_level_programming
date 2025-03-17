7baa2102352d92622f98d7998b266abdbf1666a5
Diogo <diogoanonimos@gmail.com>
2025-03-17 15:17:57 +0100

    Added 0-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module for class Square
+"""
+
+
+class Square:
+    """Class Square"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
