ca6122124a606c393aa65cc490fbe2dc38745cad
Diogo <diogoanonimos@gmail.com>
2025-03-17 15:25:54 +0100

    Added 4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,11 +6,8 @@ Module for class Square
 
 class Square:
     """Class Square"""
-    def __init__(self, size=0):
-        self.size(size)
-
-    def size(self):
-        return self.__size
+    def __init__(self, s=0):
+        self.size(s)
 
     def size(self, value):
         if type(value) is not int:
@@ -20,6 +17,9 @@ class Square:
             raise ValueError("size must be >= 0")
 
         self.__size = value
+
+    def size(self):
+        return self.__size
 
     def area(self):
         return self.__size * self.__size
