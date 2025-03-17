cb33691c9ca83689c75b8e8ee6cd1b71f2247054
Diogo <diogoanonimos@gmail.com>
2025-03-17 17:05:25 +0100

    Push

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,6 +4,12 @@ THis module defines the add_integer function
 """
 
 def add_integer(a, b=98):
+    """
+    Function to add two integers
+    :param a: Number to be added
+    :param b:  Number to be added
+    :return: Sum of two numbers
+    """
     if type(a) not in (int, float):
         raise TypeError('a must be an integer')
     if type(b) not in (int, float):
