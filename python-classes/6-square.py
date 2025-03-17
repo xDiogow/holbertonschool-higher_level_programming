fc30a5561a905757ee403b8a222da27de4419119
Diogo <diogoanonimos@gmail.com>
2025-03-17 15:50:39 +0100

    Added 6-square.py

---
+++ python-classes/6-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+Module for class Square
+"""
+
+
+class Square:
+    """Class Square"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) is not tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+        self.__position = value
+
+    def my_print(self):
+        if self.size <= 0:
+            print()
+            return
+
+        for _ in range(self.position[1]):
+            print()
+
+        for i in range(0, self.size):
+            print(self.position[0] * ' ' + '#' * self.size)
+
+    def area(self):
+        return self.size * self.size
+
