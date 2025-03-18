e7eedf103b63c2be7721529c8869497091174ea9
Diogo <diogoanonimos@gmail.com>
2025-03-18 14:08:20 +0100

    Logic

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -15,5 +15,19 @@ def text_indentation(text):
     for char in '.?:':
         text = text.replace(char, f'{char}\n')
 
+    lines = []
     for line in text.split('\n'):
-        print(line)
+        stripped_line = line.strip()
+        lines.append(stripped_line)
+
+    print("\n".join(lines), end="")
+
+text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
+Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
+Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
+Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
+rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
+stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
+cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
+beatiorem! Iam ruinas videres""")
