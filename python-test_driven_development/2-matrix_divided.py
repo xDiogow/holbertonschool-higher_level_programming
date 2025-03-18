7192524712ec6f0a45b571b605cd1f95244401ef
Diogo <diogoanonimos@gmail.com>
2025-03-18 12:44:59 +0100

    Updated 2-matrix_divided.py

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -11,7 +11,10 @@ def matrix_divided(matrix, div):
     :param div: Divisor
     :return: Divided matrix
     """
-    if not isinstance(matrix, list):
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    if not all(isinstance(element, (int, float)) for row in matrix for element in row):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
     row_size = len(matrix[0])
