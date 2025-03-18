ab9610b355d1dd194b47850f5ddb780fa0d2c9c8
Diogo <diogoanonimos@gmail.com>
2025-03-18 13:59:40 +0100

    Docs

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+"""
+This module is used for text indentation
+"""
 
 def text_indentation(text):
+    """
+    Prints each line of text by adding an empty line
+    :param text: Text to be indented
+    :return: None
+    """
     if not isinstance(text, str):
         raise TypeError('text must be a string')
 
