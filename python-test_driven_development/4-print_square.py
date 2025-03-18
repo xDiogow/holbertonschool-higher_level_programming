3b4bb215306027595a99606d6aefa3641bf345fe
Diogo <diogoanonimos@gmail.com>
2025-03-18 13:46:55 +0100

    Added 4-print_square (Added Tests & Logic)

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+"""
+This module defines the print_square function
+"""
+
+
+def print_square(size):
+    """
+    This function prints a square.
+    :param size: Size of the square.
+    :return: Nothing
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print('#' * size)
