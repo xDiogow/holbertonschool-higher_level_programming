b88fc02f68e74f01242936b80765a1e3f18d7228
Diogo <diogoanonimos@gmail.com>
2025-03-18 14:08:47 +0100

    Codestyle

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,6 +3,7 @@
 This module is used for text indentation
 """
 
+
 def text_indentation(text):
     """
     Prints each line of text by adding an empty line
@@ -21,13 +22,3 @@ def text_indentation(text):
         lines.append(stripped_line)
 
     print("\n".join(lines), end="")
-
-text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
-Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
-Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
-Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
-Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
-rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
-stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
-cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
-beatiorem! Iam ruinas videres""")
