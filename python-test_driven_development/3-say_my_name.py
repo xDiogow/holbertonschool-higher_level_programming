eb6718085243173eee2f23b297311eb01245fa38
Diogo <diogoanonimos@gmail.com>
2025-03-18 13:41:41 +0100

    Updated 3-say_my_name.py (Updated Logic)

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -10,10 +10,10 @@ def say_my_name(first_name, last_name=""):
     :param last_name: Last name.
     :return: Nothing
     """
-    if isinstance(first_name, str):
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
 
-    if isinstance(last_name, str):
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
     print(f"My name is {first_name} {last_name}", end='')
