b420ad32c20af0a95584a559b2daeecfb0a8f771
Diogo <diogoanonimos@gmail.com>
2025-03-21 09:27:28 +0100

    Added 3-list_tables.sql

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,2 +1,2 @@
 -- Get all tables available inside a database
-SELECT table_name FROM information_schema.tables WHERE table_schema=mysql;
+SELECT table_name FROM information_schema.tables WHERE table_schema='mysql';
