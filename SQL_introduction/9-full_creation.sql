0d0280757d38dde681bdc44e37776c62257ecc14
Diogo <diogoanonimos@gmail.com>
2025-03-21 09:54:52 +0100

    Added 9-full_creation.sql

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 
 -- Insert a new row in the table second_table
-INSERT INTO second_table VALUES(1, 'John', 10)
-INSERT INTO second_table VALUES(2, 'Alex', 3)
-INSERT INTO second_table VALUES(3, 'Bob', 14)
-INSERT INTO second_table VALUES(4, 'George', 8)
+INSERT INTO second_table VALUES(1, 'John', 10);
+INSERT INTO second_table VALUES(2, 'Alex', 3);
+INSERT INTO second_table VALUES(3, 'Bob', 14);
+INSERT INTO second_table VALUES(4, 'George', 8);
