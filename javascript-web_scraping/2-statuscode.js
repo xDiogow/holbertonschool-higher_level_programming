88d514bbd7825ff9d0a6cbcb2aa101826ba861f1
Diogo <diogoanonimos@gmail.com>
2025-03-21 14:55:57 +0100

    Added 2-statuscode.js

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,9 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+
   console.log('code:', response && response.statusCode);
 });
