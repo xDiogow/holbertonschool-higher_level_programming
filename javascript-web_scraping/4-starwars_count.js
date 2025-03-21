ab4e833d0bf05c5721bacb92d697c75ebd027b50
Diogo <diogoanonimos@gmail.com>
2025-03-21 15:13:32 +0100

    Added 4-starwars_count.js

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -13,9 +13,11 @@ request(url, function (error, response, body) {
 
   let amount = 0;
   for (const movie of jsonResponse.results) {
-    if (movie.characters.endsWith('/people/18/')) {
-      amount++;
-      break;
+    for (const character of movie.characters) {
+      if (character.endsWith('/18/')) {
+        amount++;
+        break;
+      }
     }
   }
   console.log(amount);
