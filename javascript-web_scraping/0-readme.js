1de058bba8fb95f6ea0b145b43ce6253471d3261
Diogo <diogoanonimos@gmail.com>
2025-03-21 14:39:12 +0100

    Indentation

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,11 +3,10 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 
-
 fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
