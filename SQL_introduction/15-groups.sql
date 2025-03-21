02efed6c5c325f4aefed96cd829e2e5d9d083380
Diogo <diogoanonimos@gmail.com>
2025-03-21 10:36:10 +0100

    Added 15-groups.sql

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Lists the number of records with the same score in the table second_table
-SELECT score, DISTINCT COUNT AS number FROM second_table;
+SELECT score, DISTINCT COUNT(score) AS number FROM second_table;
